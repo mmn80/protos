ee882e1276c4e07cc50e4d0f9e55babba2bed6b3
Călin Ardelean <mmn80cpu@gmail.com>
2023-02-24 18:51:45 +0200

    Allow hinge to rotate in both dirs; optimize query

--- src/ai/joints.rs
+++ src/ai/joints.rs
@@ -1,5 +1,5 @@
 use bevy::{prelude::*, transform::TransformSystem};
-use std::f32::consts::TAU;
+use std::f32::consts::PI;
 
 pub struct JointsPlugin;
 
@@ -23,20 +23,41 @@ pub struct KinematicHinge {
 
 impl KinematicHinge {
     pub fn get_angle(&self, transform: &Transform) -> f32 {
-        self.start_dir_up.angle_between(transform.up())
+        let sign = {
+            let dir = self.start_dir_up.cross(transform.up());
+            if dir.length() > 0.01 && dir.dot(transform.right()) < 0.01 {
+                -1.
+            } else {
+                1.
+            }
+        };
+        sign * self.start_dir_up.angle_between(transform.up())
     }
 }
 
-fn process_joints(mut q_child: Query<(&mut Transform, &KinematicHinge)>) {
-    for (mut child_tr, child_h) in &mut q_child {
-        let anchor = child_tr.transform_point(child_h.anchor);
-        let angle = child_h.get_angle(&child_tr);
-        let target_angle = child_h.target_angle.clamp(0., TAU);
+#[derive(Component)]
+pub struct KinematicHingeMoving;
+
+fn process_joints(
+    q_command: Query<Entity, Changed<KinematicHinge>>,
+    mut q_hinge: Query<(Entity, &mut Transform, &KinematicHinge), With<KinematicHingeMoving>>,
+    mut cmd: Commands,
+) {
+    for hinge in &q_command {
+        cmd.entity(hinge).insert(KinematicHingeMoving);
+    }
+
+    for (entity, mut platform_tr, hinge) in &mut q_hinge {
+        let anchor = platform_tr.transform_point(hinge.anchor);
+        let angle = hinge.get_angle(&platform_tr);
+        let target_angle = hinge.target_angle.clamp(-PI + 0.01, PI);
         let diff = target_angle - angle;
         if diff.abs() > 0.001 {
             let rotation =
-                Quat::from_axis_angle(child_h.axis, child_h.speed.min(diff.abs()) * diff.signum());
-            child_tr.rotate_around(anchor, rotation);
+                Quat::from_axis_angle(hinge.axis, hinge.speed.min(diff.abs()) * diff.signum());
+            platform_tr.rotate_around(anchor, rotation);
+        } else {
+            cmd.entity(entity).remove::<KinematicHingeMoving>();
         }
     }
 }
